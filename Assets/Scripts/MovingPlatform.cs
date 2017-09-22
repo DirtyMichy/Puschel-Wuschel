35a008af43ce3b08c58e91ecad3b777a6656ad80
DirtyMichy <michy1138@web.de>
2017-09-22 07:57:02 +0200

    Features

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public float rangeX = 4f;
+    public float rangeY = 0f;
+    public float speed = 4f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("y", rangeY, "x", rangeX, "loopType", "pingPong", "easeType", "easeInOutExpo", "speed", speed));
+    }
+}
