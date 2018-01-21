db9f38414f9e8977ae6ad7b26df54f32b5adc6b8
DirtyMichy <michy1138@web.de>
2018-01-22 00:10:26 +0100

    Xmas Content

---
+++ Assets/Santa.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Santa : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveBy(gameObject, iTween.Hash("x", 160f, "easeType", "linear", "loopType", "loop", "time", 30f ));
+	}
+}
