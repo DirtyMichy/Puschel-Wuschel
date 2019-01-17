c7455d9d0c920cf288a0dee62635ff5c1adff776
DirtyMichy <michy1138@web.de>
2019-01-17 22:10:58 +0100

    Tutorial

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    public Sprite tutorialSpeechBox;
+    public Sprite tutorialSpeechBoxNew;
+    private bool showTut = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.tag == "Player" && showTut)
+        {
+            showTut = true;
+            tutorialSpeechBox = tutorialSpeechBoxNew;
+        }
+    }
+}
