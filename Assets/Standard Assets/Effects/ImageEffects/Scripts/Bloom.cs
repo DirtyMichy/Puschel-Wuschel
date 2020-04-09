edf67c1593a6cf95fa37aa6c1e7aeacc4a9df94f
DirtyMichy <michy1138@web.de>
2020-04-09 23:36:47 +0200

    Update
    Unity 2019

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/Bloom.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/Bloom.cs
@@ -106,7 +106,7 @@ namespace UnityStandardAssets.ImageEffects
 			#pragma warning disable 0618
             doHdr = false;
 			if (hdr == HDRBloomMode.Auto) 
-				doHdr = source.format == RenderTextureFormat.ARGBHalf && GetComponent<Camera> ().hdr;			
+				doHdr = source.format == RenderTextureFormat.ARGBHalf && GetComponent<Camera> ().allowHDR;			
             else {
                 doHdr = hdr == HDRBloomMode.On;
             }
--- Assets/Standard Assets/Effects/ImageEffects/Scripts/BloomAndFlares.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/BloomAndFlares.cs
@@ -109,7 +109,7 @@ namespace UnityStandardAssets.ImageEffects
 			#pragma warning disable 0618
             doHdr = false;
             if (hdr == HDRBloomMode.Auto)
-                doHdr = source.format == RenderTextureFormat.ARGBHalf && GetComponent<Camera>().hdr;
+                doHdr = source.format == RenderTextureFormat.ARGBHalf && GetComponent<Camera>().allowHDR;
             else
             {
                 doHdr = hdr == HDRBloomMode.On;
